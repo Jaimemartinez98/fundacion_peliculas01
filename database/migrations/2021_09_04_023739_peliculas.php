b9980ef4dfb5fa2a0e6f2ed46eaf19cef202a6c0
Jaimemartinez98 <82061295+Jaimemartinez98@users.noreply.github.com>
2021-09-03 21:47:54 -0500

    03 Septiembre 2021 v2

---
+++ database/migrations/2021_09_04_023739_peliculas.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Peliculas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peliculas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_pelicula', 100);
+            $table->string('director', 100);
+            $table->date('fecha_lanzamiento');
+            $table->text('sinopsis');
+            $table->string('correo_contacto', 100);
+            $table->unsignedBigInteger('empresa_id');
+            $table->foreign('empresa_id')->references('id')->on('empresas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peliculas');
+    }
+}
